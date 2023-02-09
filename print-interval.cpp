d79c6b5398ed930381ee1abc7c7bcc5c0e469589
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-08 20:31:13 -0600

    add print-interval

---
+++ print-interval.cpp
@@ -0,0 +1,17 @@
+//Name : Irene He He
+//GitHub Name : Irene06he
+
+#include<iostream>
+
+int main(){
+ int L,U;
+  std::cout<<"Please enter L:";
+  std::cin>>L;
+  std::cout<<"Please enter U:";
+  std::cin>>U;
+  for(int i = L; i < U; i++) {
+    std::cout<<i<<" ";
+      }
+  
+  return 0;
+}
