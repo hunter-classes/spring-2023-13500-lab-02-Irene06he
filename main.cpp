84dbfe97e9ad71f5edbdb9273cbd1a4debf2111f
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-12 01:55:35 -0600

    add task b

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Name:Irene He He
+//GitHub Name: Irene06he
+
+#include <iostream>
+
+#include "funcs.h"
+
+
+int main(){
+
+print_interval(3,6);
+std::cout<<"\n\n";
+
+print_interval(-5,10);
+std::cout<<"\n\n";
+
+return 0 ;
+}
